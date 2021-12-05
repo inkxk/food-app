ca1719fe520c375f683995fc01b9f886aa0c69e4
inkxk_ <nokink@hotmail.com>
2021-12-05 23:12:36 +0700

    finish setup

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { useFonts, Montserrat_400Regular, Montserrat_500Medium, Montserrat_600SemiBold, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+import colors from './assets/colors/colors';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -16,10 +18,11 @@ export default function App() {
   } else {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ fontFamily: 'Montserrat_400Regular' }}>Montserrat_400Regular</Text>
-        <Text style={{ fontFamily: 'Montserrat_500Medium' }}>Montserrat_500Medium</Text>
-        <Text style={{ fontFamily: 'Montserrat_600SemiBold' }}>Montserrat_600SemiBold</Text>
-        <Text style={{ fontFamily: 'Montserrat_700Bold' }}>Montserrat_700Bold</Text>
+        <Text style={{ fontFamily: 'Montserrat_400Regular', color: colors.secondary }}>Montserrat_400Regular</Text>
+        <Text style={{ fontFamily: 'Montserrat_500Medium', color: colors.primary }}>Montserrat_500Medium</Text>
+        <Text style={{ fontFamily: 'Montserrat_600SemiBold', color: colors.price }}>Montserrat_600SemiBold</Text>
+        <Text style={{ fontFamily: 'Montserrat_700Bold', color: colors.textDark }}>Montserrat_700Bold</Text>
+        <Ionicons name="md-checkmark-circle" size={32} color="green" />
       </View>
     );
   }
