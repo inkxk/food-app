3336b2c43777839fd96ab986c0cbb20e56f05767
inkxk_ <nokink@hotmail.com>
2021-12-05 22:59:59 +0700

    adding custon fonts

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Montserrat_400Regular, Montserrat_500Medium, Montserrat_600SemiBold, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ fontFamily: 'Montserrat_400Regular' }}>Montserrat_400Regular</Text>
+        <Text style={{ fontFamily: 'Montserrat_500Medium' }}>Montserrat_500Medium</Text>
+        <Text style={{ fontFamily: 'Montserrat_600SemiBold' }}>Montserrat_600SemiBold</Text>
+        <Text style={{ fontFamily: 'Montserrat_700Bold' }}>Montserrat_700Bold</Text>
+      </View>
+    );
+  }
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
