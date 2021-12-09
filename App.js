a39d0b0d1aa221ef0fd6acbda85b8d57a5782769
inkxk_ <nokink@hotmail.com>
2021-12-09 15:15:39 +0700

    finish navigation

--- App.js
+++ App.js
@@ -4,6 +4,11 @@ import AppLoading from 'expo-app-loading';
 import { useFonts, Montserrat_400Regular, Montserrat_500Medium, Montserrat_600SemiBold, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 import colors from './assets/colors/colors';
 import { Ionicons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './components/Home';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -17,13 +22,11 @@ export default function App() {
     return <AppLoading />;
   } else {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ fontFamily: 'Montserrat_400Regular', color: colors.secondary }}>Montserrat_400Regular</Text>
-        <Text style={{ fontFamily: 'Montserrat_500Medium', color: colors.primary }}>Montserrat_500Medium</Text>
-        <Text style={{ fontFamily: 'Montserrat_600SemiBold', color: colors.price }}>Montserrat_600SemiBold</Text>
-        <Text style={{ fontFamily: 'Montserrat_700Bold', color: colors.textDark }}>Montserrat_700Bold</Text>
-        <Ionicons name="md-checkmark-circle" size={32} color="green" />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
---
+++ components/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default Home = () => {
+    return (
+        <Text>Hello</Text>
+    )
+}
